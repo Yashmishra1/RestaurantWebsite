b773b663e6c679cae7eb5352b3b575e2a61e3227
Yash Mishra <2018cscloudyash6851@poornima.edu.in>
2022-11-14 00:15:21 +0530

    Add logic

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+ const App = () => {
+  return (
+    <>
+    <Name/>
+    <h1 className="myclassName">Welcome to learn React js - yash Mishra </h1>
+    <p>Awesome</p>
+    </>
+  )
+}
+
+const Name = () => {
+  return <h1>Yash Mishra</h1>;
+}
+export default App;
